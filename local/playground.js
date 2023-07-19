af9d5ef8ad89de4126354661661dcebed35c3972
Swaroop Uprety <breadfan18@gmail.com>
2023-07-19 14:43:29 -0400

    add backup data and assets

---
+++ local/playground.js
@@ -0,0 +1,24 @@
+const testCard = {
+  annualFee: "595",
+  appDate: "2023-07-03",
+  card: "Platinum",
+  cardType: "Personal",
+  cardholder: "Jess Elle",
+  creditLine: "15000",
+  id: "amex-platinum-4-178b53dc9c5",
+  inquiries: {
+    equifax: true,
+    experian: false,
+    transunion: true,
+  },
+  issuer: {
+    img: "https://i.imgur.com/1XOuPt8.png",
+    name: "Amex",
+  },
+  nextFeeDate: "2023-09-02",
+  signupBonus: "100,000 Points",
+  spendBy: "2023-08-08",
+  spendReq: "5000",
+  status: "open",
+  userId: 4,
+};
