0ff7f75ae19a651127ed523f2063df11ab6c08c5
Swaroop Uprety <breadfan18@gmail.com>
2023-07-20 19:58:09 -0400

    add all loyalty programs

--- local/playground.js
+++ local/playground.js
@@ -1,41 +1,171 @@
-
-const testCard = {
-  annualFee: "595",
-  appDate: "2023-07-03",
-  card: "Platinum",
-  cardType: "Personal",
-  cardholder: "Jess Elle",
-  creditLine: "15000",
-  id: "amex-platinum-4-178b53dc9c5",
-  inquiries: {
-    equifax: true,
-    experian: false,
-    transunion: true,
+const PROGRAMS = [
+  {
+    id: "1",
+    type: "airlines",
+    name: "American Aadvantage",
+    img: "https://i.imgur.com/Pc1I7uX.png"
   },
-  issuer: {
-    img: "https://i.imgur.com/1XOuPt8.png",
-    name: "Amex",
+  {
+    id: "2",
+    type: "airlines",
+    name: "Unietd MileagePlus",
+    img: "https://i.imgur.com/znFTwQ1.png"
   },
-  nextFeeDate: "2023-09-02",
-  signupBonus: "100,000 Points",
-  spendBy: "2023-08-08",
-  spendReq: "5000",
-  status: "open",
-  userId: 4,
-};
+  {
+    id: "3",
+    type: "airlines",
+    name: "SouthWest",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "4",
+    type: "airlines",
+    name: "Delta",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "5",
+    type: "airlines",
+    name: "Turkish Airlines",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "6",
+    type: "airlines",
+    name: "Etihad Guest",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "7",
+    type: "airlines",
+    name: "British Airways",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "8",
+    type: "airlines",
+    name: "Qatar Airways",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "9",
+    type: "airlines",
+    name: "Singapore Air",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "10",
+    type: "airlines",
+    name: "Korean Skypass",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "11",
+    type: "airlines",
+    name: "Air France Flying Blue",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "12",
+    type: "airlines",
+    name: "Frontier",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "13",
+    type: "airlines",
+    name: "Asiana Airlines",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "14",
+    type: "airlines",
+    name: "Qantas",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "15",
+    type: "airlines",
+    name: "ANA Mileage Club",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "16",
+    type: "airlines",
+    name: "Alaska Airlines",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "17",
+    type: "airlines",
+    name: "Hawaiian Airlines",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "18",
+    type: "misc",
+    name: "AwardWallet",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "19",
+    type: "misc",
+    name: "Priority Pass",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "20",
+    type: "misc",
+    name: "AA eShopping",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "21",
+    type: "misc",
+    name: "Global Entry",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "22",
+    type: "misc",
+    name: "Hertz Rentals",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "23",
+    type: "hotels",
+    name: "IHG",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "24",
+    type: "hotels",
+    name: "Hilton Honors",
+    img: "https://i.imgur.com/3A83gV6.png"
+  },
+  {
+    id: "25",
+    type: "hotels",
+    name: "Club Carlson",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "26",
+    type: "hotels",
+    name: "Marriott/SPG",
+    img: "https://i.imgur.com/B7Jvm5b.png"
+  },
+  {
+    id: "27",
+    type: "hotels",
+    name: "Hyatt",
+    img: "https://i.imgur.com/UnaXElg.png"
+  }
+]
 
-// function formatDate(dateStr) {
-//   if (dateStr.length === 0) return ""
-//   const dateSplit = dateStr.split("-");
-//   const formattedMonth = dateSplit[0].length === 1 ? `0${dateSplit[0]}` : dateSplit[0] 
-//   const formattedDay = dateSplit[1].length === 1 ? `0${dateSplit[1]}` : dateSplit[1] 
-//   return `${dateSplit[2]}-${formattedMonth}-${formattedDay}`;
-// }
+const foo = PROGRAMS.map(p => ({
+  ...p,
+  id: parseInt(p.id)
+}))
 
-// console.log(formatDate('2-4-2016'));
-const arr = [1,2,3]
-const firstElem = arr.shift()
-
-arr.push(firstElem)
-
-console.log(arr);
+console.log(foo);
--- src/constants.js
+++ src/constants.js
@@ -84,35 +84,167 @@ export const ACC_STATUS = ["open", "closed", "downgraded"];
 export const PROGRAMS = [
   {
     id: 1,
-    type: "airlines",
-    name: "American AAdvantage",
-    img: "https://i.imgur.com/Pc1I7uX.png",
+    type: 'airlines',
+    name: 'American Aadvantage',
+    img: 'https://i.imgur.com/Pc1I7uX.png'
   },
   {
     id: 2,
-    type: "airlines",
-    name: "United MileaguePlus",
-    img: "https://i.imgur.com/znFTwQ1.png",
+    type: 'airlines',
+    name: 'Unietd MileagePlus',
+    img: 'https://i.imgur.com/znFTwQ1.png'
   },
   {
     id: 3,
-    type: "hotels",
-    name: "World of Hyatt",
-    img: "https://i.imgur.com/UnaXElg.png",
+    type: 'airlines',
+    name: 'SouthWest',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
   },
   {
     id: 4,
-    type: "hotels",
-    name: "Hilton Honors",
-    img: "https://i.imgur.com/3A83gV6.png",
+    type: 'airlines',
+    name: 'Delta',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
   },
   {
     id: 5,
-    type: "misc",
-    name: "Global Entry",
-    img: "",
+    type: 'airlines',
+    name: 'Turkish Airlines',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
   },
-];
+  {
+    id: 6,
+    type: 'airlines',
+    name: 'Etihad Guest',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 7,
+    type: 'airlines',
+    name: 'British Airways',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 8,
+    type: 'airlines',
+    name: 'Qatar Airways',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 9,
+    type: 'airlines',
+    name: 'Singapore Air',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 10,
+    type: 'airlines',
+    name: 'Korean Skypass',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 11,
+    type: 'airlines',
+    name: 'Air France Flying Blue',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 12,
+    type: 'airlines',
+    name: 'Frontier',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 13,
+    type: 'airlines',
+    name: 'Asiana Airlines',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 14,
+    type: 'airlines',
+    name: 'Qantas',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 15,
+    type: 'airlines',
+    name: 'ANA Mileage Club',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 16,
+    type: 'airlines',
+    name: 'Alaska Airlines',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 17,
+    type: 'airlines',
+    name: 'Hawaiian Airlines',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 18,
+    type: 'misc',
+    name: 'AwardWallet',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 19,
+    type: 'misc',
+    name: 'Priority Pass',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 20,
+    type: 'misc',
+    name: 'AA eShopping',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 21,
+    type: 'misc',
+    name: 'Global Entry',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 22,
+    type: 'misc',
+    name: 'Hertz Rentals',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 23,
+    type: 'hotels',
+    name: 'IHG',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 24,
+    type: 'hotels',
+    name: 'Hilton Honors',
+    img: 'https://i.imgur.com/3A83gV6.png'
+  },
+  {
+    id: 25,
+    type: 'hotels',
+    name: 'Club Carlson',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 26,
+    type: 'hotels',
+    name: 'Marriott/SPG',
+    img: 'https://i.imgur.com/B7Jvm5b.png'
+  },
+  {
+    id: 27,
+    type: 'hotels',
+    name: 'Hyatt',
+    img: 'https://i.imgur.com/UnaXElg.png'
+  }
+]
 
 export const USERS = [
   {
