25d2da3aae983ac855ad16c3ad965ea0d747e502
Swaroop Uprety <breadfan18@gmail.com>
2023-07-24 20:07:05 -0400

    optimize test card generator

--- src/ts/createTestCard.js
+++ src/ts/createTestCard.js
@@ -1,25 +1,38 @@
 import { uid } from "uid";
 import { slugify } from "../helpers";
 import { writeToFirebase } from "../tools/firebase";
+import { ISSUERS } from "../constants";
+
+// Random Date
+function getRandomDate(startDate, endDate) {
+  const timeDiff = endDate.getTime() - startDate.getTime();
+  const randomTime = Math.random() * timeDiff;
+  const randomDate = new Date(startDate.getTime() + randomTime);
+  return randomDate.toISOString().slice(0, 10);
+}
+
+// Random Issuer
+const randomIssuer = ISSUERS[Math.floor(Math.random() * ISSUERS.length)];
+
+// Random Card Type
+const CARD_TYPE = ["Personal", "Business"];
+const randomCardType = CARD_TYPE[Math.floor(Math.random() * CARD_TYPE.length)];
 
 const testCard = {
-  appDate: "2023-01-01",
-  issuer: {
-    name: "Chase",
-    img: "https://i.imgur.com/AsfYKFY.png",
-  },
-  card: "Test Card",
-  cardType: "Personal",
+  appDate: getRandomDate(new Date("2021-08-24"), new Date()),
+  issuer: randomIssuer,
+  card: "FOO",
+  cardType: randomCardType,
   inquiries: {
     experian: true,
     equifax: true,
     transunion: false,
   },
   annualFee: 95,
-  nextFeeDate: "2024-01-02",
+  nextFeeDate: getRandomDate(new Date("2021-08-24"), new Date()),
   creditLine: "5000",
   spendReq: "3000",
-  spendBy: "2023-04-15",
+  spendBy: getRandomDate(new Date("2021-08-24"), new Date()),
   signupBonus: "50,000 UR",
   bonusEarned: false,
   bonusEarnDate: "",
