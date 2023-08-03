91082c248380e5cf8617e82029cac0e6064a1f4e
Swaroop Uprety <breadfan18@gmail.com>
2023-08-03 16:44:24 -0400

    minor style optimization

--- src/components/common/UserProfileSection.js
+++ src/components/common/UserProfileSection.js
@@ -11,28 +11,26 @@ export default function UserProfileSection({ user, windowWidth }) {
     history.push("/signin");
   }
   return (
-    <div className="userInfo">
-      <section id="userImg">
-        <img
-          src={user.photoURL}
-          alt=""
-          style={{
-            borderRadius: "50%",
-            height: "2rem",
-            border: "2px solid white",
-          }}
-          title={user.displayName}
-        />
-        <GoSignOut
-          onClick={handleSignOut}
-          style={{
-            fontSize: "1.8rem",
-            cursor: "pointer",
-            marginLeft: "5px",
-          }}
-          title="Sign Out"
-        />
-      </section>
-    </div>
+    <section id="userImg">
+      <img
+        src={user.photoURL}
+        alt=""
+        style={{
+          borderRadius: "50%",
+          height: "2rem",
+          border: "2px solid white",
+        }}
+        title={user.displayName}
+      />
+      <GoSignOut
+        onClick={handleSignOut}
+        style={{
+          fontSize: "1.8rem",
+          cursor: "pointer",
+          marginLeft: "5px",
+        }}
+        title="Sign Out"
+      />
+    </section>
   );
 }
