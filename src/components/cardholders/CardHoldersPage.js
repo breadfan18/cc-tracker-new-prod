88ebfe03b948659f47a088566f13deb64639e0f1
Swaroop Uprety <breadfan18@gmail.com>
2023-08-25 13:23:21 -0400

    blah bleh

--- src/components/cardholders/CardHoldersPage.js
+++ src/components/cardholders/CardHoldersPage.js
@@ -4,11 +4,17 @@ import { connect, useSelector } from "react-redux";
 import { loadCardholdersFromFirebase } from "../../redux/actions/cardholderActions";
 import { useUser } from "reactfire";
 import { foo } from "../../tools/firebase";
+import { Button, Form } from "react-bootstrap";
+import SelectInput from "../common/SelectInput";
+import TextInput from "../common/TextInput";
 
 const CardHoldersPage = ({ loadCardholdersFromFirebase }) => {
   const { status, data: user } = useUser();
-
   const cardholders = useSelector((state) => state.cardholders);
+
+  const handleChange = (e) => {
+    console.log("fdoo");
+  };
 
   useEffect(() => {
     if (cardholders.length === 0 && status !== "loading") {
@@ -22,9 +28,18 @@ const CardHoldersPage = ({ loadCardholdersFromFirebase }) => {
     <div className="cardHoldersContainer">
       <h2 className="sectionHeaders">
         <h2 style={{ marginBottom: 0 }}>Manage Card Holders</h2>
-        <CardHolderAddEditModal />
       </h2>
       {elems}
+      <TextInput
+        name="Test"
+        label="Card Holder Name"
+        value={""}
+        onChange={handleChange}
+        // error={errors.title}
+      />
+      <Button className="addButton" onClick={() => {}}>
+        Add
+      </Button>
     </div>
   );
 };
