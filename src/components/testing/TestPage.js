eeacdda3f43421e75d98e07a1d78bb3d4de7f229
Swaroop Uprety <breadfan18@gmail.com>
2023-09-06 11:41:49 -0400

    s3 code

--- src/components/testing/TestPage.js
+++ src/components/testing/TestPage.js
@@ -1,73 +1,53 @@
 import React, { useState } from "react";
+import AWS from "aws-sdk";
 
 export default function TestPage() {
-  const [file, setFile] = useState();
-
-  const onFileChange = (event) => {
-    // Updating the state
-    setFile({ file: event.target.files[0] });
+  const [file, setFile] = useState(null);
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setFile(file);
   };
 
-  const onFileUpload = async () => {
-    // Client ID
-    // const clientId = "dfcd98b52a00d5e",
-    //   auth = "Client-ID " + clientId;
+  const uploadFile = async () => {
+    const S3_BUCKET = "cc-tracker";
+    const REGION = "US West (N. California) us-west-1";
 
-    // // Creating an object of formData
-    // const formData = new FormData();
+    AWS.config.update({
+      accessKeyId: "AKIAR2TT5OLODYESQ7NQ",
+      secretAccessKey: "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",
+    });
+    const s3 = new AWS.S3({
+      params: { Bucket: S3_BUCKET },
+      region: REGION,
+    });
 
-    // // Adding our image to formData
-    // formData.append("file", file);
-
-    // console.log(formData);
-
-    // // Making the post request
-    // await fetch("https://api.imgur.com/3/image/", {
-    //   // API Endpoint
-    //   method: "POST", // HTTP Method
-    //   body: formData, // Data to be sent
-    //   headers: {
-    //     // Setting header
-    //     Authorization: "Bearer c04e54608aacc45274b28ffac9c465fece504f06",
-    //     Accept: "application/json",
-    //   },
-    // })
-    //   .then((res) => alert("image uploaded") && console.log(res)) // Handling success
-    //   .catch((err) => alert("Failed") && console.log(err)); // Handling error
-
-    var myHeaders = new Headers();
-    myHeaders.append(
-      "Authorization",
-      "Bearer 78275d66f39bdbafd1fd58b46ffe7728e0227af4"
-    );
-    myHeaders.append(
-      "Cookie",
-      "IMGURSESSION=e75437027621d0bccedd945efde64dc7; _nc=1"
-    );
-
-    var formdata = new FormData();
-    formdata.append("image", file);
-
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: formdata,
-      redirect: "follow",
+    const params = {
+      Bucket: S3_BUCKET,
+      Key: file.name,
+      Body: file,
     };
 
-    fetch("https://api.imgur.com/3/image", requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error));
+    var upload = s3
+      .putObject(params)
+      .on("httpUploadProgress", (evt) => {
+        console.log(
+          "Uploading " + parseInt((evt.loaded * 100) / evt.total) + "%"
+        );
+      })
+      .promise();
+
+    await upload.then((err, data) => {
+      console.log(err);
+      alert("File uploaded successfully.");
+    });
   };
 
-  console.log(file);
-
   return (
-    <>
-      <h2>TEST PAGE</h2>
-      <input type="file" onChange={onFileChange} />
-      <button onClick={onFileUpload}>Upload</button>
-    </>
+    <div className="foo">
+      <div>
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={uploadFile}>Upload</button>
+      </div>
+    </div>
   );
 }
