03f87091b8ce359f6cd8f6504109df0fe5e3afc1
Swaroop Uprety <breadfan18@gmail.com>
2024-10-28 16:27:18 -0400

    notifications actions

---
+++ src/redux/actions/notificationsActions.js
@@ -0,0 +1,36 @@
+import { beginApiCall } from "./apiStatusActions";
+import {
+  LOAD_NOTIFICATIONS_SUCCESS,
+  DELETE_NOTIFICATION_SUCCESS,
+} from "./actionTypes";
+import { deleteFromFirebase, getFireBaseData } from "../../tools/firebase";
+
+function loadNotificationsSuccess(notifications) {
+  return { type: LOAD_NOTIFICATIONS_SUCCESS, notifications };
+}
+
+function deleteNotificationSuccess(notification) {
+  return { type: DELETE_NOTIFICATION_SUCCESS, notification };
+}
+
+export function loadNotificationsFromFirebase(firebaseUid) {
+  return (dispatch) => {
+    dispatch(beginApiCall());
+    getFireBaseData(
+      "notifications",
+      dispatch,
+      loadNotificationsSuccess,
+      firebaseUid
+    );
+  };
+}
+
+export function deleteNotificationFromFirebase(notification, firebaseUid) {
+  return (dispatch) => {
+    // Same reason to dispatch apiCall twice here as mentioned above in save function
+    dispatch(beginApiCall());
+    dispatch(beginApiCall());
+    deleteFromFirebase("notifications", notification.id, firebaseUid);
+    dispatch(deleteNotificationSuccess(notification));
+  };
+}
